038107990d7cef887205b93ef377826fe1fb8bf2
sailexx <52277096+sailexx@users.noreply.github.com>
2023-05-09 13:48:30 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
 const WebSocket = require('ws');
 
-const server = new WebSocket.Server({ port: 8080 });
+const server = new WebSocket.Server({ port: 4000 });
 console.clear()
 var arr = [] 
 var connectionCount = 0;
+var msgArr = []
 
 server.on('connection', (socket) => {
+    socket.on('message', (msg)=>{ 
+        msgArr.push(msg.toString);
+        console.log('Client msg count: ' + msgArr.length);
+    })
+
     arr.push(socket);
     connectionCount++;
     console.log('Client connected count: ' + arr.length);
 
     // Send the current time to the client every second
     const interval = setInterval(() => {
+        
+
         var date = new Date();
 
         var dateStr = `${date.getFullYear()}/${date.getMonth()}/${date.getDay()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
-        socket.send(JSON.stringify(dateStr));
+        socket.send(JSON.stringify("hello"));
 
     }, 1000);
 
