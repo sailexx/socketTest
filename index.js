761d1ded12928677ca7cc1f9326f50ca53e40f92
sailexx <52277096+sailexx@users.noreply.github.com>
2023-05-15 10:48:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let errorMsg = "";
 
 const connectionNumber = Number(prompt('Enter MAX_CONNECTIONS: '));
 const MAX_CONNECTIONS = isNaN(connectionNumber) || connectionNumber === 0
-  ? 100
+  ? 600
   : connectionNumber;
 
 const intervalId = setInterval(() => {
@@ -20,19 +20,36 @@ const intervalId = setInterval(() => {
 
   const id = wsArr.length;
   const ws = new WebSocket(SERVER_URL);
-  const wo = { id, ws, newMsg: "", errMsg: "", hasError: false };
+  const wo = createWebSocket(id, ws);
+
   wsArr.push(wo);
 
+  // setInterval(() => {
+  //   if (wo.ws.readyState !== WebSocket.OPEN) {
+  //     console.log('Retrying the connection..');
+  //     const newWs = new WebSocket(SERVER_URL);
+  //     const newWo = createWebSocket(wo.id, newWs);
+  //     wsArr[id] = newWo; // update the wsArr array with the new WebSocket object
+  //   }
+  // }, MAX_CONNECTIONS * 100);
+
+}, 100);
+
+
+function createWebSocket(id, ws) {
+  const wo = { id, ws, newMsg: "", errMsg: "", hasError: false };
+
   ws.on('open', () => { });
-  ws.on('close', () => removeClient(wo));
+
   ws.on('message', (message) => {
     wo.hasError = false;
     wo.newMsg = message.toString();
     if (msg !== message.toString()) {
       msg = message.toString();
-   
+
     }
   });
+
   ws.on('error', (error) => {
     wo.hasError = true;
     wo.errMsg = error.toString();
@@ -40,19 +57,35 @@ const intervalId = setInterval(() => {
       errorMsg = error.toString();
     }
   });
-}, 100);
+
+  ws.on('close', () => { });
+
+  return wo;
+
+}
+
+
+
+
+
+
+
+
+
+
 
 setInterval(() => {
   console.clear();
   console.log("Received MSG        : " + msg);
   console.log("Is all the msg same : " + allSame(wsArr.map((w) => w.newMsg)));
   console.log("total ws count  : " + wsArr.length);
+  console.log(" ")
 
-  console.log("all msg data")
+  console.log("all msg data: ")
 
   printCount(wsArr.map((w) => w.newMsg));
-
-  console.log("all error data")
+  console.log(" ")
+  console.log("all error datas: ")
 
   printCount(wsArr.map((w) => w.errMsg));
 }, 50);
@@ -83,107 +116,7 @@ function printCount(arr) {
     countMap[item] = countMap[item] ? countMap[item] + 1 : 1;
   }
   for (const [item, count] of Object.entries(countMap)) {
-    console.log(`${item} * ${count}`);
+    if(item !== "") console.log(`${item} * ${count}`);
   }
 }
 
-
-// const WebSocket = require('ws');
-// const prompt = require('prompt-sync')({ sigint: true });
-
-// const SERVER_URL = 'ws://127.0.0.1:4000';
-
-// let MAX_CONNECTIONS = 10;
-// let wsArr = [];
-// var id = "";
-// var errMsgId = "";
-// var errMsg = "";
-// var msg = "";
-
-
-// const connectionNumber = Number(prompt('Enter MAX_CONNECTIONS: '));
-
-// if (!isNaN(connectionNumber) && connectionNumber !== 0) {
-//   MAX_CONNECTIONS = connectionNumber
-// }
-
-
-
-// let intervalId = setInterval(() => {
-//   if (wsArr.length >= MAX_CONNECTIONS) {
-//     return clearInterval(intervalId);
-//   }
-
-//   const wo = addNewClient();
-
-
-//   wo.ws.on('open', () => { });
-
-//   wo.ws.on('close', () => {
-//     removeClient(wo);
-//   });
-
-//   wo.ws.on('message', (message) => {
-//     wo.newMsg = message.toString();
-
-//     if (msg !== message.toString()) {
-//       msg = message.toString();
-//       id = wo.id + "";
-//     }
-//   });
-
-//   wo.ws.on('error', (error) => {
-//     errMsg = error.toString();
-//     errMsgId = wo.id + "";
-//     wo.newMsg = errMsg
-//   });
-//   wsArr.push(wo);
-// }, 20);
-
-// setInterval(() => {
-//   console.clear()
-//   console.log("msg from ws id      : " + id);
-//   console.log("Received MSG        : " + msg);
-//   // check if all the msg is same 
-//   console.log("Is all the msg same : " + allSame(wsArr))
-
-//   console.log("total ws count  : " + wsArr.length)
-//   console.log("error MsgId     : " + errMsgId)
-//   console.log("error Msg       : " + errMsg)
-//   printCount(wsArr.map((w) => w.newMsg))
-// }, 10);
-
-// function addNewClient() {
-//   const id = wsArr.length;
-//   const ws = new WebSocket(SERVER_URL);
-//   return { id: id, ws: ws, newMsg: "" };
-// }
-
-// function removeClient(wo) {
-//   wsArr = wsArr.filter((item) => item !== wo);
-// }
-
-// process.on('SIGINT', () => {
-//   console.log('Closing all connections');
-//   wsArr.forEach((wo) => wo.ws.close());
-//   process.exit();
-// });
-
-// function allSame(arr) {
-//   for (let i = 1; i < arr.length; i++) {
-//     if (arr[i].newMsg !== arr[0].newMsg) {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
-
-// function printCount(arr) {
-//   const countMap = {};
-//   for (const item of arr) {
-//     countMap[item] = countMap[item] ? countMap[item] + 1 : 1;
-//   }
-//   for (const [item, count] of Object.entries(countMap)) {
-//     console.log(`${item} * ${count}`);
-//   }
-// }
